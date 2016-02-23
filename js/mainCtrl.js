86992e8b57e7ec6982937773cdd17f611a2b7a54
Erica Andrew <erica.nicole.andrew@gmail.com>
2016-02-22 19:20:53 -0700

    Code complete - 404 Error on given urls

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,3 +1,5 @@
+/* global angular */
+
 var app = angular.module('chatroom');
 
 app.controller('mainCtrl', function($scope, parseService){
@@ -6,17 +8,23 @@ app.controller('mainCtrl', function($scope, parseService){
   //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
   //your controllers $scope as messages ($scope.messages)
 
+    $scope.getParseData = function(){
+        parseService.getData();
+    };
 
 
   //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
 
-
+    $scope.postData = function(input) {
+        console.log("postData ran");
+        parseService.postData(input);
+    };
 
 
   //uncomment this code when your getParseData function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1500)
-})
+   setInterval(function(){
+     $scope.getParseData();
+   }, 1500);
+});
